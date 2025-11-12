38016c81f500aafcd851b4c702fe03edfcaa0cde
Suman <isuman0007@gmail.com>
2025-11-13 00:12:21 +0530

    homescreen data not showing fixd

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,8 @@ class MyApp extends StatelessWidget {
           initialSemester: semester,
           initialBatch: batch,
           initialBranch: branch,
+
+          
         ),
       },
     );
